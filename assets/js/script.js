a87fcfe2621970896af5064213ad613810d8a496
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-10-21 19:53:18 +0700

    fix: remove jquery from all scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,75 @@
-jQuery(document).ready(function ($) {
+const ready = function (cb) {
+  document.readyState === "loading"
+    ? document.addEventListener("DOMContentLoaded", function () {
+      cb();
+    })
+    : cb();
+};
 
+/**********************************************************************
+ * Back to top functionality
+ *********************************************************************/
+ready(function () {
   'use strict';
-
-  // back to top link
   let offset = 220;
-  let duration = 500;
-  jQuery('body').append('<a href="#" id="back-to-top">Zur&uuml;ck nach oben</a>');
-  jQuery(window).scroll(function () {
-      if (jQuery(this).scrollTop() > offset) {
-          jQuery('#back-to-top').fadeIn(duration);
-      } else {
-          jQuery('#back-to-top').fadeOut(duration);
-      }
+  let backToTopLink = document.createElement('a');
+  backToTopLink.href = '#';
+  backToTopLink.id = 'back-to-top';
+  backToTopLink.innerHTML = 'Zur&uuml;ck nach oben';
+  document.body.appendChild(backToTopLink);
+  window.addEventListener('scroll', function () {
+    let backToTopLinkDiv = document.getElementById('back-to-top');
+    let location = document.body.getBoundingClientRect();
+    if (Math.abs(location.top) > offset) {
+      backToTopLinkDiv.classList.add('d-block');
+      backToTopLinkDiv.classList.remove('d-none');
+    } else {
+      backToTopLinkDiv.classList.add('d-none');
+      backToTopLinkDiv.classList.remove('d-block');
+    }
   });
-  jQuery('#back-to-top').on('click', function (event) {
-      event.preventDefault();
-      //jQuery('html, body').animate({scrollTop: 0}, 1000, 'swing');
-      window.scroll({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-      });
-      return false;
+  document.getElementById('back-to-top').addEventListener('click', function () {
+    window.scroll({top: 0, left: 0, behavior: 'smooth'});
+    return false;
   });
+});
 
-  jQuery('.is-datediff').each(function(){
-    let $this = $(this);
-    let $date1 = moment($this.data('from'));
+/**********************************************************************
+ * Datediff shortcode functionality
+ *********************************************************************/
+ready(function () {
+  'use strict';
+  let elements = document.querySelectorAll('.is--datediff');
+  Array.prototype.forEach.call(elements, function (el) {
+    let $date1 = moment(el.getAttribute('data-date'));
     let $date2 = moment(new Date());
-    $this.text($date2.diff($date1, 'days')+1);
-  });
-
-  jQuery('.is-datediff-month').each(function(){
-    let $this = $(this);
-    let $date1 = moment($this.data('from'));
-    let $date2 = moment(new Date());
-    $this.text(parseInt($date2.diff($date1, 'months')));
-  });
-
-  jQuery('.is--datediff').each(function(){
-      let $this = $(this);
-      let $date1 = moment($this.data('date'));
-      let $date2 = moment(new Date());
-      let $type = $this.data('type');
-      if ($type === 'months'){
-          $this.text(parseInt($date2.diff($date1, 'months')));
-      } else {
-          $this.text($date2.diff($date1, 'days')+1);
-      }
-  });
-
-  // cookieconsent
-  // https://cookieconsent.osano.com/documentation/javascript-api/
-  window.cookieconsent.initialise({
-    "theme": "edgeless",
-    "position": "bottom-right",
-    "type": "info",
-    "revokable": false,
-    "content": {
-      "message": "Auf dieser Webseite werden Cookies genutzt, um Funktionen und Vorg&auml;nge zu gew&auml;hrleisten, die seit Jahrzehnten so im Internet funktioniert haben.",
-      "allow": "Yeah, Cookies!!!1Eins",
-      "dismiss": "Yeah, Cookies!!!1Eins",
-      "deny": "Keine Cookies f&uuml;r mich.",
-      "link": "Weitere Informationen",
-      "href": "/datenschutzerklaerung/"
+    let $type = el.getAttribute('data-type');
+    if ($type === 'months') {
+      el.textContent = parseInt($date2.diff($date1, 'months'));
+    } else {
+      el.textContent = $date2.diff($date1, 'days') + 1;
     }
   });
 });
 
-// make navbar static on scroll
-let bottom = false;
-jQuery(window).scroll(function () {
+/**********************************************************************
+ * make navbar static on scroll
+ *********************************************************************/
+ready(function () {
+  'use strict';
+  let bottom = false;
+  window.addEventListener('scroll', function () {
     'use strict';
-    let menu = jQuery('#topnavigation');
-    let offset = menu.offset();
+    let menu = document.getElementById('topnavigation');
+    let offset = menu.getBoundingClientRect();
     if (bottom === false) {
-        bottom = offset.top;
+      bottom = offset.top;
     }
-    if (jQuery(window).scrollTop() > bottom) {
-        menu.addClass('navbar-fixed-top');
+    let bodyOffset = window.pageYOffset || document.documentElement.scrollTop;
+    if (bodyOffset > bottom) {
+      menu.classList.add('navbar-fixed-top');
     } else {
-        menu.removeClass('navbar-fixed-top');
+      menu.classList.remove('navbar-fixed-top');
     }
+  });
 });
