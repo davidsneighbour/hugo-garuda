7d8ed5524ebbbb3e2482a43165f80909f4826745
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-10-29 19:46:00 +0700

    chore: remove dnb stuff from the theme

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -1,7 +1,5 @@
-const ready = function (cb) {
-  document.readyState === "loading"
-    ? document.addEventListener("DOMContentLoaded", function () {
-      cb();
-    })
+const ready=function(cb){
+  document.readyState==="loading"
+    ? document.addEventListener("DOMContentLoaded",function(){cb();})
     : cb();
 };
