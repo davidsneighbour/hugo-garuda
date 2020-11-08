d2b55f32d4b9cb91600f564c39da597765d3288e
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-11-08 20:00:07 +0700

    layouts: i18n of sidebar and fixes

---
+++ assets/js/bootstrap.js
@@ -0,0 +1,7 @@
+// Import just what we need
+
+// If you're importing tooltips or popovers, be sure to include the Popper.js dependency
+// import "../../node_modules/popper.js/dist/popper.min.js";
+
+import 'libs/bootstrap/js/dist/util.js';
+import Alert from 'libs/bootstrap/js/dist/alert';
