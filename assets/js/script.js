7f9ff1e52914620d0698d6771220e5cfaf7d80bf
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-12-18 22:59:09 +0700

    refactor: fix codingstyle

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,26 @@
  * Back to top functionality
  *********************************************************************/
 ready(function () {
-  'use strict';
+  "use strict";
   let offset = 220;
-  let backToTopLink = document.createElement('a');
-  backToTopLink.href = '#';
-  backToTopLink.id = 'back-to-top';
-  backToTopLink.innerHTML = 'Zur&uuml;ck nach oben';
+  let backToTopLink = document.createElement("a");
+  backToTopLink.href = "#";
+  backToTopLink.id = "back-to-top";
+  backToTopLink.innerHTML = "Zur&uuml;ck nach oben";
   document.body.appendChild(backToTopLink);
-  window.addEventListener('scroll', function () {
-    let backToTopLinkDiv = document.getElementById('back-to-top');
+  window.addEventListener("scroll", function () {
+    let backToTopLinkDiv = document.getElementById("back-to-top");
     let location = document.body.getBoundingClientRect();
     if (Math.abs(location.top) > offset) {
-      backToTopLinkDiv.classList.add('d-block');
-      backToTopLinkDiv.classList.remove('d-none');
+      backToTopLinkDiv.classList.add("d-block");
+      backToTopLinkDiv.classList.remove("d-none");
     } else {
-      backToTopLinkDiv.classList.add('d-none');
-      backToTopLinkDiv.classList.remove('d-block');
+      backToTopLinkDiv.classList.add("d-none");
+      backToTopLinkDiv.classList.remove("d-block");
     }
   });
-  document.getElementById('back-to-top').addEventListener('click', function () {
-    window.scroll({top: 0, left: 0, behavior: 'smooth'});
+  document.getElementById("back-to-top").addEventListener("click", function () {
+    window.scroll({top: 0, left: 0, behavior: "smooth"});
     return false;
   });
 });
@@ -30,16 +30,16 @@ ready(function () {
  * Datediff shortcode functionality
  *********************************************************************/
 ready(function () {
-  'use strict';
-  let elements = document.querySelectorAll('.is--datediff');
+  "use strict";
+  let elements = document.querySelectorAll(".is--datediff");
   Array.prototype.forEach.call(elements, function (el) {
-    let $date1 = moment(el.getAttribute('data-date'));
+    let $date1 = moment(el.getAttribute("data-date"));
     let $date2 = moment(new Date());
-    let $type = el.getAttribute('data-type');
-    if ($type === 'months') {
-      el.textContent = parseInt($date2.diff($date1, 'months'));
+    let $type = el.getAttribute("data-type");
+    if ($type === "months") {
+      el.textContent = parseInt($date2.diff($date1, "months"));
     } else {
-      el.textContent = $date2.diff($date1, 'days') + 1;
+      el.textContent = $date2.diff($date1, "days") + 1;
     }
   });
 });
@@ -48,20 +48,20 @@ ready(function () {
  * make navbar static on scroll
  *********************************************************************/
 ready(function () {
-  'use strict';
+  "use strict";
   let bottom = false;
-  window.addEventListener('scroll', function () {
-    'use strict';
-    let menu = document.getElementById('topnavigation');
+  window.addEventListener("scroll", function () {
+    "use strict";
+    let menu = document.getElementById("topnavigation");
     let offset = menu.getBoundingClientRect();
     if (bottom === false) {
       bottom = offset.top;
     }
     let bodyOffset = window.pageYOffset || document.documentElement.scrollTop;
     if (bodyOffset > bottom) {
-      menu.classList.add('navbar-fixed-top');
+      menu.classList.add("navbar-fixed-top");
     } else {
-      menu.classList.remove('navbar-fixed-top');
+      menu.classList.remove("navbar-fixed-top");
     }
   });
 });
--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -14,9 +14,9 @@ class LiteYTEmbed extends HTMLElement {
   connectedCallback() {
     // Gotta encode the untrusted value
     // https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#rule-2---attribute-escape-before-inserting-untrusted-data-into-html-common-attributes
-    this.videoId = encodeURIComponent(this.getAttribute('videoid'));
-    const playLabel = this.getAttribute('playlabel');
-    this.playLabel = playLabel ? encodeURIComponent(playLabel) : 'Play';
+    this.videoId = encodeURIComponent(this.getAttribute("videoid"));
+    const playLabel = this.getAttribute("playlabel");
+    this.playLabel = playLabel ? encodeURIComponent(playLabel) : "Play";
 
     /**
      * Lo, the youtube placeholder image!  (aka the thumbnail, poster image, etc)
@@ -25,7 +25,7 @@ class LiteYTEmbed extends HTMLElement {
      *
      * amp-youtube also eschews using the API, so they just try sddefault with a hqdefault fallback:
      *   https://github.com/ampproject/amphtml/blob/6039a6317325a8589586e72e4f98c047dbcbf7ba/extensions/amp-youtube/0.1/amp-youtube.js#L498-L537
-     * For now I'm gonna go with this confident (lol) assertion: https://stackoverflow.com/a/20542029, though I'll use `i.ytimg` to optimize for origin reuse.
+     * For now I"m gonna go with this confident (lol) assertion: https://stackoverflow.com/a/20542029, though I"ll use `i.ytimg` to optimize for origin reuse.
      *
      * Worth noting that sddefault is _higher_ resolution than hqdefault. Naming is hard. ;)
      * From my own testing, it appears that hqdefault is ALWAYS there sddefault is missing for ~10% of videos
@@ -36,28 +36,28 @@ class LiteYTEmbed extends HTMLElement {
      */
     this.posterUrl = `https://i.ytimg.com/vi/${this.videoId}/hqdefault.jpg`;
     // Warm the connection for the poster image
-    LiteYTEmbed.addPrefetch('preload', this.posterUrl, 'image');
+    LiteYTEmbed.addPrefetch("preload", this.posterUrl, "image");
 
 
     this.style.backgroundImage = `url("${this.posterUrl}")`;
 
-    let playBtn = this.querySelector('.lty-playbtn');
+    let playBtn = this.querySelector(".lty-playbtn");
 
     if (!playBtn) {
-      playBtn = document.createElement('button');
-      playBtn.type = 'button';
-      playBtn.classList.add('lty-playbtn');
+      playBtn = document.createElement("button");
+      playBtn.type = "button";
+      playBtn.classList.add("lty-playbtn");
       playBtn.title = decodeURIComponent(this.playLabel);
       this.append(playBtn);
     }
 
-    // On hover (or tap), warm up the TCP connections we're (likely) about to use.
-    this.addEventListener('pointerover', LiteYTEmbed.warmConnections, {once: true});
+    // On hover (or tap), warm up the TCP connections we"re (likely) about to use.
+    this.addEventListener("pointerover", LiteYTEmbed.warmConnections, {once: true});
 
     // Once the user clicks, add the real iframe and drop our play button
     // TODO: In the future we could be like amp-youtube and silently swap in the iframe during idle time
-    //   We'd want to only do this for in-viewport or near-viewport ones: https://github.com/ampproject/amphtml/pull/5003
-    this.addEventListener('click', e => this.addIframe());
+    //   We"d want to only do this for in-viewport or near-viewport ones: https://github.com/ampproject/amphtml/pull/5003
+    this.addEventListener("click", e => this.addIframe());
   }
 
   // // TODO: Support the the user changing the [videoid] attribute
@@ -68,7 +68,7 @@ class LiteYTEmbed extends HTMLElement {
    * Add a <link rel={preload | preconnect} ...> to the head
    */
   static addPrefetch(kind, url, as) {
-    const linkElem = document.createElement('link');
+    const linkElem = document.createElement("link");
     linkElem.rel = kind;
     linkElem.href = url;
     if (as) {
@@ -79,39 +79,39 @@ class LiteYTEmbed extends HTMLElement {
 
   /**
    * Begin pre-connecting to warm up the iframe load
-   * Since the embed's network requests load within its iframe,
-   *   preload/prefetch'ing them outside the iframe will only cause double-downloads.
+   * Since the embed"s network requests load within its iframe,
+   *   preload/prefetch"ing them outside the iframe will only cause double-downloads.
    * So, the best we can do is warm up a few connections to origins that are in the critical path.
    *
-   * Maybe `<link rel=preload as=document>` would work, but it's unsupported: http://crbug.com/593267
-   * But TBH, I don't think it'll happen soon with Site Isolation and split caches adding serious complexity.
+   * Maybe `<link rel=preload as=document>` would work, but it"s unsupported: http://crbug.com/593267
+   * But TBH, I don"t think it"ll happen soon with Site Isolation and split caches adding serious complexity.
    */
   static warmConnections() {
     if (LiteYTEmbed.preconnected) return;
 
     // The iframe document and most of its subresources come right off youtube.com
-    LiteYTEmbed.addPrefetch('preconnect', 'https://www.youtube-nocookie.com');
+    LiteYTEmbed.addPrefetch("preconnect", "https://www.youtube-nocookie.com");
     // The botguard script is fetched off from google.com
-    LiteYTEmbed.addPrefetch('preconnect', 'https://www.google.com');
+    LiteYTEmbed.addPrefetch("preconnect", "https://www.google.com");
 
     // Not certain if these ad related domains are in the critical path. Could verify with domain-specific throttling.
-    LiteYTEmbed.addPrefetch('preconnect', 'https://googleads.g.doubleclick.net');
-    LiteYTEmbed.addPrefetch('preconnect', 'https://static.doubleclick.net');
+    LiteYTEmbed.addPrefetch("preconnect", "https://googleads.g.doubleclick.net");
+    LiteYTEmbed.addPrefetch("preconnect", "https://static.doubleclick.net");
 
     LiteYTEmbed.preconnected = true;
   }
 
   addIframe(){
-    const params = new URLSearchParams(this.getAttribute('params') || []);
-    params.append('autoplay', '1');
+    const params = new URLSearchParams(this.getAttribute("params") || []);
+    params.append("autoplay", "1");
     const iframeHTML = `
 <iframe class="w-100" frameborder="0"
   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
   src="https://www.youtube-nocookie.com/embed/${this.videoId}?${params.toString()}"
 ></iframe>`;
-    this.insertAdjacentHTML('beforeend', iframeHTML);
-    this.classList.add('lyt-activated');
+    this.insertAdjacentHTML("beforeend", iframeHTML);
+    this.classList.add("lyt-activated");
   }
 }
 // Register custome element
-customElements.define('lite-youtube', LiteYTEmbed);
+customElements.define("lite-youtube", LiteYTEmbed);
