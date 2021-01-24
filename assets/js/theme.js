822efb4c288d3df3b1bdd60d4cab5662ef26ae17
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-01-24 21:33:06 +0700

    feat: js.Build integration and moving lazyloading images into theme js

---
+++ assets/js/theme.js
@@ -0,0 +1,5 @@
+import lazySizes from 'libs/lazysizes';
+
+// https://github.com/aFarkas/lazysizes
+// needs to be called explicitly, because webpack can't see into uncompiled hugo
+lazySizes.init();
