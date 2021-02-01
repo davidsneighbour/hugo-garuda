bca7b6c4f135979c39c8fcdffc1324bb4cae42ee
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-02-01 23:06:58 +0700

    chore: remove obsolete files

--- assets/js/service-worker.js
+++
@@ -1,5 +0,0 @@
-// if('serviceWorker' in navigator) {
-//   const registerServiceWorker = async () =>
-//     navigator.serviceWorker.register('./service-worker.js');
-//   const registration = await registerServiceWorker();
-// }
